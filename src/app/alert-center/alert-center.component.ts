a3bd5094ba0e5663e9659a2deeeebf449fb50924
Murat Colyaran <muratcolyaran@yahoo.com.tr>
2020-07-09 14:13:20 +0300

    Alert center visual improvements. --- Demo version

--- src/app/alert-center/alert-center.component.ts
+++ src/app/alert-center/alert-center.component.ts
@@ -1,19 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-import { faLaptop, faMailBulk, faTrash, faPen } from '@fortawesome/free-solid-svg-icons';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
+import {
+  faLaptop,
+  faMailBulk,
+  faTrash,
+  faPen,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-alert-center',
   templateUrl: './alert-center.component.html',
-  styleUrls: ['./alert-center.component.css']
+  styleUrls: ['./alert-center.component.css'],
 })
 export class AlertCenterComponent implements OnInit {
   faLaptop = faLaptop;
   faMail = faMailBulk;
   faTrash = faTrash;
   faPen = faPen;
-  constructor() { }
+  alertTab: boolean = false;
+  selectedCurrency: string;
+  alertSection: string;
+  alertOptions: string;
+  constructor(private el: ElementRef, private renderer: Renderer2) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  openAlertTab(value) {
+    value == 'Choose...' ? this.alertTab = false : this.alertTab = true;
+
+    if (this.alertTab) {
+      this.alertSection = this.el.nativeElement.querySelector(
+        'body > main > app-alert-center > div > div'
+      );
+      this.alertOptions = this.el.nativeElement.querySelector()
+      this.renderer.addClass(this.alertSection, 'alertTab');
+    } else {
+      this.renderer.removeClass(this.alertSection, 'alertTab');
+    }
+    this.alertTab = true;
   }
-
 }

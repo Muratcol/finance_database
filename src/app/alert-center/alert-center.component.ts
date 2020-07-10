fa117ca35f653ce71276ecf5a3cae18e8819922b
Murat Colyaran <muratcolyaran@yahoo.com.tr>
2020-07-10 14:12:42 +0300

    Alert table updated, made dynamic--- Demo version

--- src/app/alert-center/alert-center.component.ts
+++ src/app/alert-center/alert-center.component.ts
@@ -4,6 +4,7 @@ import {
   faMailBulk,
   faTrash,
   faPen,
+  faArrowRight,
 } from '@fortawesome/free-solid-svg-icons';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Alert } from './alertModel';
@@ -20,63 +21,66 @@ export class AlertCenterComponent implements OnInit {
   faMail = faMailBulk;
   faTrash = faTrash;
   faPen = faPen;
+  faArrow = faArrowRight;
   alertTab: boolean = false;
   selectedCurrency: string;
   alertSection: string;
   alertOptions: string;
   alertForm: FormGroup;
   alert: Alert = new Alert();
-  radios:any;
-  username:string;
-  webPopup:any;
-  emailNotify:any;
+  radios: any;
+  username: string;
+  webPopup: any;
+  emailNotify: any;
+  userName: string;
+  allAlerts: Alert[];
   constructor(
     private el: ElementRef,
     private renderer: Renderer2,
     private formBuilder: FormBuilder,
     private alertifyService: AlertifyService,
-    private alertService:AlertService
+    private alertService: AlertService
   ) {}
 
   ngOnInit(): void {
     this.createAlertForm();
+    this.userName = localStorage.getItem('name');
+    this.printAlerts();
+    console.log(this.allAlerts)
   }
 
-    createAlertForm() {
-      
-      this.alertForm = this.formBuilder.group ({
-        pair: [null, Validators.required],
-        limit: [null, Validators.required],
-        conditionName: [null, Validators.required],
-        frequency: [null],
-        websitePopup: [null],
-        emailNotify: [null]
-      })
+  createAlertForm() {
+    this.alertForm = this.formBuilder.group({
+      userName: [null],
+      pair: [null, Validators.required],
+      limit: [null, Validators.required],
+      conditionName: [null, Validators.required],
+      frequency: [null],
+      websitePopup: [null],
+      emailNotify: [null],
+    });
+  }
+
+  submitAlert() {
+    this.radios = this.el.nativeElement.querySelectorAll('.radio');
+    this.webPopup = this.el.nativeElement.querySelector('.webPopup');
+    this.emailNotify = this.el.nativeElement.querySelector('.emailNotify');
+    this.alertForm.value.userName = this.userName;
+    this.radios[0].checked
+      ? (this.alertForm.value.frequency = 'Once')
+      : (this.alertForm.value.frequency = 'Requirring');
+    this.alertForm.value.websitePopup = this.webPopup.checked;
+    this.alertForm.value.emailNotify = this.emailNotify.checked;
+    if (this.alertForm.valid) {
+      this.alert = Object.assign({}, this.alertForm.value);
+      this.printAlerts();
+      this.alertService.createAlert(this.alert).subscribe(() => {
+        this.alertifyService.success('Alert created. Thank you');
+      });
+    } else {
+      this.alertifyService.error('Please check your inputs.');
     }
-
-    submitAlert() {
-      this.radios = this.el.nativeElement.querySelectorAll('.radio');
-      this.webPopup = this.el.nativeElement.querySelector('.webPopup');
-      this.emailNotify = this.el.nativeElement.querySelector('.emailNotify');
-      this.radios[0].checked ? this.alertForm.value.frequency = "Once": this.alertForm.value.frequency = "Requirring"
-      this.alertForm.value.websitePopup = this.webPopup.checked
-      this.alertForm.value.emailNotify = this.emailNotify.checked
-      if (this.alertForm.valid){
-        this.alert = Object.assign({}, this.alertForm.value)
-        this.alertService.createAlert(this.alert)
-        .subscribe(() => {
-          this.alertifyService.success('Alert created. Thank you')
-        })
-      }
-      else {
-        console.log(this.alertForm.value)
-        this.alertifyService.error("Please check your inputs.")
-      }
-      
-    }
-
-
-
+  }
 
   openAlertTab(value) {
     if (value != 'Choose...') {
@@ -95,5 +99,11 @@ export class AlertCenterComponent implements OnInit {
       }, 2000);
     }
     this.alertTab = true;
+  }
+
+  printAlerts() {
+    this.alertService.getAlerts().subscribe((data) => {
+      this.allAlerts = data['data'];
+    });
   }
 }
--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,58 +1,70 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+} from '@angular/common/http';
 import { throwError, Observable } from 'rxjs';
 import { tap, catchError } from 'rxjs/operators';
 import { Alert } from '../alert-center/alertModel';
 
 @Injectable()
-
 export class AlertService {
-    path:string = "http://localhost:5000"
-  
-    constructor(
-      private http:HttpClient
-    ) { }
-  
-  
-    createAlert (alertForm:Alert):Observable<Alert[]> {
-  
-      let access_token = localStorage.getItem('access_token')
+  path: string = 'http://localhost:5000';
 
-      let http_options = {
-        headers: new HttpHeaders ({
-          Authorization: 'Bearer: ' + access_token
-        })
-        
-      }
-      const body = {
-        "username" : localStorage.getItem('name'),
-        'pair': alertForm.pair,
-        'limit':alertForm.limit,
-        'conditionName':alertForm.conditionName,
-        'frequency':alertForm.frequency,
-        'websitePopup':alertForm.websitePopup,
-        'emailNotify':alertForm.emailNotify
-      }
-  
-  
-      return this.http
+  constructor(private http: HttpClient) {}
+
+  createAlert(alertForm: Alert): Observable<Alert[]> {
+    let access_token = localStorage.getItem('access_token');
+
+    let http_options = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer: ' + access_token,
+      }),
+    };
+    const body = {
+      userName: alertForm.userName,
+      pair: alertForm.pair,
+      limit: alertForm.limit,
+      conditionName: alertForm.conditionName,
+      frequency: alertForm.frequency,
+      websitePopup: alertForm.websitePopup,
+      emailNotify: alertForm.emailNotify,
+    };
+
+    return this.http
       .post<Alert[]>(this.path + '/alert/createAlert', body, http_options)
       .pipe(
-        tap(data => console.log(JSON.stringify(data))),
+        tap((data) => console.log(JSON.stringify(data))),
         catchError(this.handleError)
-      )
-      
+      );
+  }
+
+  getAlerts():Observable<Alert[]> {
+    let access_token = localStorage.getItem('access_token');
+
+    let http_options = {
+      headers: new HttpHeaders({
+        Authorization: 'Bearer: ' + access_token,
+      }),
+    };
+
+    return this.http
+    .get<Alert[]>(this.path + "/alert/getAllAlerts", http_options)
+    .pipe(
+      tap(),
+      catchError(err => this.handleError(err))
+    )
+  }
+
+
+  handleError(err: HttpErrorResponse) {
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      errorMessage = 'Somethings not right ' + err.error.message;
+    } else {
+      errorMessage = 'Internal Error';
     }
-  
-    handleError(err: HttpErrorResponse) {
-      let errorMessage = ""
-      if(err.error instanceof ErrorEvent) {
-        errorMessage = 'Somethings not right ' + err.error.message
-      }
-      else {
-        errorMessage = "Internal Error"
-      }
-      return throwError(errorMessage)
-    }
-    
+    return throwError(errorMessage);
+  }
 }

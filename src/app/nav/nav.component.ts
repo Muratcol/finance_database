4c97af4bd4b050670559d78b57249362f4f85aa5
Murat Colyaran <muratcolyaran@yahoo.com.tr>
2020-07-18 15:31:42 +0300

    Alert popup window implemented

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,32 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, Renderer2 } from '@angular/core';
 import { UserService } from '../services/user.service';
 import { AlertifyService } from '../services/alertify.service';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import {
+  faBell,
+  faCog,
+  faExclamationTriangle,
+  faArrowAltCircleUp,
+  faCaretUp,
+} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
-  styleUrls: ['./nav.component.css']
+  styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  faBell = faBell
+  faBell = faBell;
+  faCog = faCog;
+  faTriangle = faCaretUp;
+  windowStatus: boolean = false;
   constructor(
-    private userService:UserService,
-    private alertifyService:AlertifyService
+    private userService: UserService,
+    private alertifyService: AlertifyService,
+    private el: ElementRef,
+    private renderer: Renderer2
   ) {}
-  
-  isLoggedIn():boolean{
-    return this.userService.isLoggedIn
+
+  isLoggedIn(): boolean {
+    return this.userService.isLoggedIn;
   }
 
-  logOut(){
-    this.userService.isLoggedIn = false
-    this.userService.logOutUser()
-    this.alertifyService.success('LogOut Succesfull')
+  logOut(): void {
+    this.userService.isLoggedIn = false;
+    this.userService.logOutUser();
+    this.alertifyService.success('LogOut Succesfull');
   }
+  showAlertWrapper(): void {
+    let alertWindow = this.el.nativeElement.querySelector(
+      '.alertWindowWrapper'
+    );
+    if (this.windowStatus) {   
+      this.renderer.addClass(alertWindow, 'slideToHide')
+      setTimeout(() => {
+        this.renderer.removeClass(alertWindow, 'slide');
+        this.windowStatus = !this.windowStatus;
+      }, 400)
 
-  ngOnInit(): void {
-
+    } else {
+      this.renderer.removeClass(alertWindow, 'slideToHide')
+      this.renderer.addClass(alertWindow, 'slide');
+      this.windowStatus = !this.windowStatus;
+    }
   }
-
+  ngOnInit(): void {}
 }
